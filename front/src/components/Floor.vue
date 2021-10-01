ffb0b0bc6d8ce90b3ea475d3592687b845dd6b69
木村龍太郎 <v-kryutaro@alterbooth.net>
2021-10-01 22:59:51 +0900

    wip

--- front/src/components/Floor.vue
+++ front/src/components/Floor.vue
@@ -1,29 +1,158 @@
 <template>
-  <v-app>
-    <v-bottom-navigation :value="value" color="primary" horizontal>
-      <v-btn>
-        <span>Recents</span>
-
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Favorites</span>
-
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Nearby</span>
-
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
+  <v-app class="body">
+    <v-app-bar app>
+      1階
+    </v-app-bar>
+    <v-main class="main">
+      <v-container class="container">
+        <div class="sec1"></div>
+        <div class="sec2"></div>
+      </v-container>
+    </v-main>
   </v-app>
+  <!-- <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table> -->
 </template>
 
 <script>
-export default {
-  data: () => ({ value: 1 }),
-};
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
+      }
+    },
+    mounted: function() {
+      this.$vuetify.application.bottom = 0;
+    },
+  }
 </script>
+
+<style scoped>
+.body {
+  height: 100%;
+}
+.main {
+  /* min-height: calc(100% - 56px);
+  height: calc(100% - 56px);
+  max-height: calc(100% - 56px); */
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  max-height: 100%;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  /* max-height: calc(100% - 56px); */
+}
+.sec1 {
+  background-color: red;
+  flex: 1;
+}
+.sec2 {
+  background-color: blue;
+  /* flex: 1; */
+  height: 200px;
+  min-height: 200px;
+  bottom: 0;
+}
+</style>
