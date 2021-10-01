15a8135db016684233887cfdfd4041aaed7b53b9
木村龍太郎 <v-kryutaro@alterbooth.net>
2021-10-01 17:22:02 +0900

    [add] Create Vuetify environments.
    Co-authored-by: mutta-n <mutta-n@users.noreply.github.com>

--- front/src/components/Floor.vue
+++ front/src/components/Floor.vue
@@ -1,5 +1,29 @@
 <template>
- <div>
-   Floor
- </div>
+  <v-app>
+    <v-bottom-navigation :value="value" color="primary" horizontal>
+      <v-btn>
+        <span>Recents</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({ value: 1 }),
+};
+</script>
