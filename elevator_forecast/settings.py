9067a2188b171aa8cb1a8a2b32d8bcabb4b32819
tomo6300 <tomoyagishita@icloud.com>
2021-10-01 08:41:37 +0000

    add API document

--- elevator_forecast/settings.py
+++ elevator_forecast/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'app.apps.AppConfig',
+    'app.apps.MyAppConfig',
 ]
 
 MIDDLEWARE = [
